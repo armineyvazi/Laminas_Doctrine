eb37a683aae9c55ae50284039ec370a6cfa2efe9
Armin Eyvazi <genius2001armin@gmail.com>
2022-08-10 21:31:20 +0430

    Add xmlrequest in indexAction Method

--- module/Post/src/Controller/PostController.php
+++ module/Post/src/Controller/PostController.php
@@ -4,17 +4,21 @@
 
 namespace Post\Controller;
 
-use Post\Entity\Post;
+
 use Post\Form\PostForm;
+use Post\Form\SearchForm;
 use Post\Service\PostManager;
 use Doctrine\ORM\EntityManager;
+use Masterminds\HTML5\Exception;
+use Laminas\View\Model\JsonModel;
 use Laminas\View\Model\ViewModel;
 use Laminas\Mvc\Controller\AbstractActionController;
-use Masterminds\HTML5\Exception;
 
 class PostController extends AbstractActionController
 {
+
     protected PostManager $postManager;
+
     protected EntityManager $entityManager;
 
     public function __construct(EntityManager $entityManager, PostManager $postManager)
@@ -25,59 +29,67 @@ class PostController extends AbstractActionController
 
     public function indexAction()
     {
-        $posts = $this->entityManager->getRepository(Post::class)->findAll();
-        return new ViewModel(\compact('form', 'posts'));
+        $form = new SearchForm;
+        $posts= $this->postManager->getAllPost();
+        $request = $this->getRequest(); 
+        $query = $request->getQuery();    
+        if($request->isXmlHttpRequest() || $query->get('showJson') == 1)
+        {
+            $query = $request->getQuery(); 
+            $jsonData=[];
+            $count=0;
+            $search = $_POST['search'];
+            $posts = $this->postManager->searchPost($search);
+            $view = new JsonModel($posts); 
+            $view->setTerminal(true); 
+        }
+        else { 
+            $posts = $this->postManager->getAllPost();
+            $view = new ViewModel(compact('form','posts')); 
+        }   
+       return $view;
     }
-
     public function addAction()
     {
         $form = new PostForm;
-
         if ($this->getRequest()->isPost()) {
             $data = $this->params()->fromPost();
             $form->setData($data);
             if ($form->isValid()) {
-
                 $data = $form->getData();
-                $this->postManager->createPost($data);
-                $this->flashMessenger()->addSuccessMessage('Album created successfully.');
+                $this->postManager->savePost($data);
+                $this->flashMessenger()->addSuccessMessage('Post Created Successfully.');
                 return $this->redirect()->toRoute('post');
             }
         }
         return new ViewModel(\compact('form'));
     }
-
     public function deleteAction()
     {
         $id = $this->params()->fromRoute('id');
-
         $this->postManager->deletePost($id);
-
+        $this->flashMessenger()->addSuccessMessage('Post Deleted Successfully.');
         return $this->redirect()->toRoute('post');
     }
-
     public function editAction()
     {
         $form = new PostForm;
         $id = $this->params()->fromRoute('id');
-
         if ($this->getRequest()->isGet()) {
             $post = $this->postManager->find($id);
             $form->bind($post);
-
             return new ViewModel(\compact('form', 'post'));
-        }
-        else if ($this->getRequest()->isPost()) {
+        } else if ($this->getRequest()->isPost()) {
             $data = $this->params()->fromPost();
             $form->setData($data);
             if (!$form->isValid()) {
+
                 throw new Exception('nist aqa nist');
             }
             $data = $form->getData();
-            $this->postManager->editPost($id, $data);
-
+            $this->postManager->savePost( $data,$id);
+            $this->flashMessenger()->addSuccessMessage('Post Edited Successfully.');
             return $this->redirect()->toRoute('post');
         }
-    }
-
+    }   
 }
